146cd627ba06e271a1f7eda97e489ce222e86154
ShotaTamaru <s2022022@stu.musashino-u.ac.jp>
2023-08-17 17:28:42 +0900

    カウントアプリアレンジSTART

--- Count/Count/ViewController.swift
+++ Count/Count/ViewController.swift
@@ -13,15 +13,35 @@ class ViewController: UIViewController {
     
     @IBOutlet var label: UILabel!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    func ifnumber(){
+        if number > 10{
+            label.textColor = UIColor.red
+        } else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    
     @IBAction func plus(){
         number += 1
         label.text = String(number)
+        
+        ifnumber()
+        
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+        
+        ifnumber()
     }
 
 
