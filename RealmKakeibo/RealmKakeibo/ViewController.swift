f9ab6716bcc813c9b36c633f713464d1a6ee1eb0
ShotaTamaru <s2022022@stu.musashino-u.ac.jp>
2023-08-26 15:59:33 +0900

    RealmSTART

---
+++ RealmKakeibo/RealmKakeibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmKakeibo
+//
+//  Created by 田丸翔大 on 2023/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
